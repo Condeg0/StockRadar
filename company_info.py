70f3aee4599a786b67df3e5e3c5ed96eda5660f1
Condeg0 <118837512+Condeg0@users.noreply.github.com>
2024-06-21 07:31:54 -0400

    Update company_info.py
    Added 12M return to relevant_info_dict and began to set up the function to get graph based on price history

--- company_info.py
+++ company_info.py
@@ -1,22 +1,39 @@
 import yfinance as yf
+import pandas
+
+STOCK = "aapl"
 
 
 # define function to get information about the stock
 def get_info(ticker):
     stock = yf.Ticker(ticker)
 
-    # create a list and a dict to hold all relevant values
     # create a list and a dict to hold all relevant values - Valuation, Efficiency and Financials (in order)
     relevant_info_list = ["dividendYield", "trailingPE", "priceToSalesTrailing12Months", "revenuePerShare",
                           "priceToBook", "returnOnAssets", "debtToEquity", "returnOnEquity", "totalRevenue",
-                          "totalDebt", "freeCashflow",  "profitMargins", "marketCap", "industry", "industryKey"]
+                          "totalDebt", "freeCashflow", "profitMargins", "marketCap", "industry", "industryKey", "open"]
     relevant_info_dict = {}
 
     # add financial information to the dictionary
     for item in relevant_info_list:
         relevant_info_dict[item] = stock.info[item]
 
+    # creates a pandas dataframe for holding price history (1Y)
+    price_hist = stock.history(period="1Y")
+    year_return = (price_hist["Close"].iloc[0] / price_hist["Close"].iloc[-1]) - 1 # calculates 12M return
+    relevant_info_dict["yearlyReturn"] = year_return
+
     return relevant_info_dict
 
 
-print(get_info("aapl"))
+# Defines function to get the graph based on two arguments - ticker and time frame (1d, 1w, 1m, 12m, 5y, all)
+def get_graph(ticker, tf):
+    stock = yf.Ticker(ticker)
+
+    # Creates a pandas dataframe for holding price history information for (1Y)
+    price_hist = stock.history(period="1Y")
+
+    # Generates graph ===== JP =====
+    graph = 0   # jpeg file or other file format
+
+    return graph
