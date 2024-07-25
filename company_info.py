47f77b67de53708f46c877aa3ea2acde27724ae0
Condeg0 <118837512+Condeg0@users.noreply.github.com>
2024-07-25 17:44:07 -0400

    Update company_info.py

--- company_info.py
+++ company_info.py
@@ -1,5 +1,5 @@
 import yfinance as yf
-import pandas
+import matplotlib.pyplot as mtp
 
 STOCK = "aapl"
 
@@ -15,30 +15,33 @@ def get_info(ticker):  # add try except statement
     relevant_info_dict = {}
 
     # add financial information to the dictionary
+    # error handling for missing keys
     for item in relevant_info_list:
-        relevant_info_dict[item] = stock.info[item]
+        try:
+            relevant_info_dict[item] = stock.info[item]
+        except KeyError:
+            relevant_info_dict[item] = "NA"
 
     # creates a pandas dataframe for holding price history (1Y)
     price_hist = stock.history(period="1Y")
     year_return = (price_hist["Close"].iloc[0] / price_hist["Close"].iloc[-1]) - 1 # calculates 12M return
     relevant_info_dict["yearlyReturn"] = year_return
 
+
     return relevant_info_dict
 
 
 # ===== JP =====
-# pandas - data frame manipulation (may not be necessary)
+# pandas - data frame manipulation
 # yfinance library - price history
 # # obs: get dataframe, clean it (remove unnecessary columns) and generate an image me frame (1d, 1w, 1m, 12m, 5y, all)
 # Defines function to get the graph based on two arguments - ticker and tf
- # example - call get_graph function from company_info.py with two arguments - stock name (ticker) and the time-
- # frame (tf). time frame selected can be retrieved by calling event.widget.cget("text"), which gets the name of
- # button  pressed- check doc
 def get_graph(ticker, tf):
     stock = yf.Ticker(ticker)
 
     # Creates a pandas dataframe for holding price history information for (1Y)
-    price_hist = stock.history(period="1Y")
+    price_hist = stock.history(period=tf)
+    # === mtp.plot() ==== E
 
     # Generates graph ===== JP =====
     graph = 0   # jpeg file or other file format
